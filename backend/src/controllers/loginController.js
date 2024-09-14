ec1f039cece4757429279a819657268ab3131bd5
joburbanop <joburbanop589@gmail.com>
2024-09-14 14:33:18 -0500

    registro he inicio de seccion con asignacion por defecto de rol

--- backend/src/controllers/loginController.js
+++ backend/src/controllers/loginController.js
@@ -4,17 +4,32 @@ const db = require('../config/db');
 const dotenv = require('dotenv');
 dotenv.config();
 
+
 exports.registerUser = (req, res) => {
   const { nombre_usuario, email, contraseña } = req.body;
-  const hashedPassword = bcrypt.hashSync(contraseña, 8);
+  const encriptacion = bcrypt.hashSync(contraseña, 8);
 
-  const sql = `INSERT INTO Usuarios (nombre_usuario, email, contraseña) VALUES (?, ?, ?)`;
-  db.query(sql, [nombre_usuario, email, hashedPassword], (err, result) => {
-    if (err) {
-      console.error('Error en la consulta:', err);
-      return res.status(500).send({ message: 'Error en el servidor', error: err });
+  // Insertar el nuevo usuario en la tabla 'usuarios'
+  const insertarusuario = `INSERT INTO Usuarios (nombre_usuario, email, contraseña) VALUES (?, ?, ?)`;
+  db.query(insertarusuario, [nombre_usuario, email, encriptacion], (error, result) => {
+    //console.log(error)
+    if (error) {
+      return res.status(500).send({ message: 'Error en el servidor al registrar usuario' });
     }
-    res.status(200).send({ message: 'Usuario registrado exitosamente' });
+    
+    const userId = result.insertId;  
+    console.log("usuraio id", userId)
+    
+    const rolId = 4;  
+
+    const insersionSQLRol = `INSERT INTO Usuario_rol (id_Usarios, id_Roles) VALUES (?, ?)`;
+    db.query(insersionSQLRol, [userId, rolId], (error, result) => {
+      if (error) {
+        return res.status(500).send({ message: 'Error al asignar rol por defecto' });
+      }
+
+      res.status(200).send({ message: 'Usuario registrado exitosamente con rol asignado' });
+    });
   });
 };
 
@@ -26,11 +41,18 @@ exports.loginUser = (req, res) => {
   }
 
   const sql = `SELECT * FROM Usuarios WHERE email = ?`;
-  db.query(sql, [email], (err, result) => {
-    if (err || result.length === 0) return res.status(404).send({ message: 'Usuario no encontrado' });
+  //console.log(sql)
+  db.query(sql, [email], (error, result) => {
+    console.log(result)
+    console.log(result[0].id_Usarios)
+    console.log(error)
+    if (result[0].id_Usarios==1 ){
+      console.log("es admin")
+    }
+    if (error || result.length === 0) return res.status(404).send({ message: 'Usuario no encontrado' });
 
-    const validPassword = bcrypt.compareSync(contraseña, result[0].contraseña);
-    if (!validPassword) return res.status(401).send({ token: null, message: 'Contraseña incorrecta' });
+    const desencriptar = bcrypt.compareSync(contraseña, result[0].contraseña);
+    if (!desencriptar) return res.status(401).send({ token: null, message: 'Contraseña incorrecta' });
 
     const token = jwt.sign({ id: result[0].id_Usuarios }, process.env.SECRET_KEY, { expiresIn: 86400 });
     res.status(200).send({ token });
